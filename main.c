61644f9904da6a964d2804d58f89cd20dd189c6c
Samyak-2212 <naskar.samyak@gmail.com>
2024-07-20 14:44:36 +0530

    Added float support and a delay upon closing program.

--- main.c
+++ main.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include<string.h>
-int add(int a, int b){
+#include<windows.h>
+float add(float a, float b){
     return a+b;
 }
-int sub(int a, int b){
+float sub(float a, float b){
     return a-b;
 }
-int mult(int a, int b){
+float mult(float a, float b){
     return a*b;
 }
-int div(int a, int b){
+float divs(float a, float b){
     return a/b;
 }
-int rem(int a, int b){
+float rem(int a, int b){
     return a%b;
 }
 
-int main(){
-    int a;
-    int b;
+float main(){
+    float a;
+    float b;
     char opp[20];
     
     printf("This is a calculator \n");
 
     printf("Enter your first number:");
-    scanf("%d", &a);
+    scanf("%f", &a);
 
     printf("Enter your second number:");
-    scanf("%d", &b);
+    scanf("%f", &b);
 
     printf("Enter operation(add, sub, mult, div, remainder):");
     scanf("%s", &opp);
@@ -35,26 +36,27 @@ int main(){
 
     if ((strcmp(opp, "add") == 0)||(strcmp(opp, "+") == 0)||(strcmp(opp, "adiition") == 0))
     {
-        printf("%d", add(a,b));
+        printf("%f", add(a,b));
     }
     else if ((strcmp(opp, "sub") == 0)||(strcmp(opp, "-") == 0)||(strcmp(opp, "subtract") == 0))
     {
-        printf("%d", sub(a,b));
+        printf("%f", sub(a,b));
     }
     else if ((strcmp(opp, "mult") == 0)||(strcmp(opp, "*") == 0)||(strcmp(opp, "x") == 0))
     {
-        printf("%d", mult(a,b));
+        printf("%f", mult(a,b));
     }
     else if ((strcmp(opp, "div") == 0)||(strcmp(opp, "divide") == 0)||(strcmp(opp, "/") == 0))
     {
-        printf("%d", div(a,b));
+        printf("%f", divs(a,b));
     }
     else if ((strcmp(opp, "rem") == 0)||(strcmp(opp, "remainder") == 0)||(strcmp(opp, "%") == 0))
     {
-        printf("%d", rem(a,b));
+        printf("%d", rem((int)a,b));
     }
     else{
         printf("Wrong opperator!");
     }   
+    Sleep(5000);
     return 0;
 }
